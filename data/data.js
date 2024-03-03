6453976ab7e05912e153f6dd5dde18ca8ce023f6
Dayas K.R <dayaskr2005@gmail.com>
2024-03-03 21:57:50 +0530

    Add assets and data folder

---
+++ data/data.js
@@ -0,0 +1,219 @@
+export const icons = {
+  downArrow: "./assets/icons/down-arrow.svg",
+  history: "./assets/icons/history.svg",
+  originals: "./assets/icons/originals.svg",
+  shopping: "./assets/icons/shopping.svg",
+  voiceSearchIcon: "./assets/icons/voice-search-icon.svg",
+  youtubeLogo: "./assets/icons/youtube-logo.svg",
+  explore: "./assets/icons/explore.svg",
+  home: "./assets/icons/home.svg",
+  podcasts: "./assets/icons/podcasts.svg",
+  shorts: "./assets/icons/shorts.svg",
+  watchLater: "./assets/icons/watch-later.svg",
+  youtubeMusic: "./assets/icons/youtube-music.svg",
+  fashionBeauty: "./assets/icons/fashion-beauty.svg",
+  library: "./assets/icons/library.svg",
+  reportHistory: "./assets/icons/report-history.svg",
+  sport: "./assets/icons/sport.svg",
+  yourChannel: "./assets/icons/your-channel.svg",
+  youtubePremium: "./assets/icons/youtube-premium.svg",
+  films: "./assets/icons/films.svg",
+  live: "./assets/icons/live.svg",
+  rightArrow: "./assets/icons/right-arrow.svg",
+  subscriptions: "./assets/icons/subscriptions.svg",
+  yourCourses: "./assets/icons/your-courses.svg",
+  youtubeStudio: "./assets/icons/youtube-studio.svg",
+  gaming: "./assets/icons/gaming.svg",
+  music: "./assets/icons/music.svg",
+  search: "./assets/icons/search.svg",
+  trending: "./assets/icons/trending.svg",
+  yourVideos: "./assets/icons/your-videos.svg",
+  hamburgerMenu: "./assets/icons/hamburger-menu.svg",
+  news: "./assets/icons/news.svg",
+  sendFeeback: "./assets/icons/send-feeback.svg",
+  upload: "./assets/icons/upload.svg",
+  youtubeApps: "./assets/icons/youtube-apps.svg",
+  help: "./assets/icons/help.svg",
+  notifications: "./assets/icons/notifications.svg",
+  settings: "./assets/icons/settings.svg",
+  verifiedIcon: "./assets/icons/verified-icon.svg",
+  youtubeKids: "./assets/icons/youtube-kids.svg",
+};
+
+// Keyword Bar
+export const keywordBarLinks = [
+  "All",
+  "Gaming",
+  "AI",
+  "Mixes",
+  "Computer Science",
+  "Editing",
+  "Podcasts",
+  "Photography",
+  "Apple",
+  "News",
+  "History",
+  "Electrical",
+  "Live",
+  "Thoughts",
+  "Film criticisms",
+  "Reading",
+  "Tamil Cinema",
+  "Computer Hardware",
+  "Recently uploaded",
+  "Watched",
+  "New to you",
+];
+
+// Default tabs
+export const defaultPageNav = [
+  {
+    name: "Home",
+    icon: icons.home,
+  },
+  {
+    name: "Subscriptions",
+    icon: icons.subscriptions,
+  },
+  {
+    name: "Shorts",
+    icon: icons.shorts,
+  },
+];
+
+// You
+export const yourTabsNav = [
+  {
+    icon: icons.yourChannel,
+    name: "Your channel",
+  },
+  {
+    icon: icons.history,
+    name: "History",
+  },
+  {
+    icon: icons.yourVideos,
+    name: "Your videos",
+  },
+  {
+    icon: icons.watchLater,
+    name: "Watch later",
+  },
+  {
+    icon: icons.downArrow,
+    name: "Show more",
+  },
+];
+
+// Subscriptions
+export const subscriptionsNav = [
+  {
+    channelName: "Adam’s English Lessons · engVid",
+    channelImage:
+      "https://yt3.ggpht.com/PSZEGJzWuLE8Ru5WasmK_QLjYerUSdKoC2qDF1Xw2_mjmoxIkyvrzd2ifzVRjQRTNC9NP483=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "Learn English with Gill · engVid",
+    channelImage:
+      "https://yt3.ggpht.com/I-AN7MnyVSpSTskNk76kksvZiw0kysvNTASuWqTdY9YRlzc7NhCVZ_FwM7ZT7MqPOX4SZqybEUM=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "English Jade · Learn English with engVid",
+    channelImage:
+      "https://yt3.ggpht.com/_wFOZ8IXOFEtN7gWExyqEkn1JmYdj8ltp6sfdbJzeV6980IrUFlFNaMrorcURxmWlw9JcLgNxg=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "Syntax",
+    channelImage:
+      "https://yt3.ggpht.com/_wFOZ8IXOFEtN7gWExyqEkn1JmYdj8ltp6sfdbJzeV6980IrUFlFNaMrorcURxmWlw9JcLgNxg=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "Gawx Art",
+    channelImage:
+      "https://yt3.ggpht.com/ytc/AIdro_kLD6cccVzFWIueAxKHFQVMNdumbHYmNTDeawNEtQ=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "JavaScript Mastery",
+    channelImage:
+      "https://yt3.ggpht.com/wg1TITEoPfxvBGfzuqWyt3bqm_qu35ZhMswUv3feetU3xNX_6wsAXZF40OlPIgY4TmqbqCmAZ1U=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "Fireship",
+    channelImage:
+      "https://yt3.ggpht.com/ytc/AIdro_ltOWCZT10fChupyd1atupxII0RoP97CwYGr0Gphw=s88-c-k-c0x00ffffff-no-rj",
+  },
+  // Show 403 more
+];
+
+// Explore
+export const exploreNav = [
+  { name: "Trending", icon: icons.trending },
+  { name: "Shopping", icon: icons.shopping },
+  { name: "Music", icon: icons.music },
+  { name: "Movies", icon: icons.films },
+  { name: "Live", icon: icons.live },
+  { name: "Gaming", icon: icons.gaming },
+  { name: "News", icon: icons.news },
+  { name: "Sports", icon: icons.sport },
+  { name: "Learning", icon: icons.yourCourses },
+  { name: "Fashion & Beauty", icon: icons.fashionBeauty },
+  { name: "Podcasts", icon: icons.podcasts },
+];
+
+// More from YouTube
+export const moreFromYouTubeNav = [
+  {
+    name: "YouTube Premium",
+    icon: icons.youtubePremium,
+  },
+  {
+    name: "YouTube Studio",
+    icon: icons.youtubeStudio,
+  },
+  {
+    name: "YouTube Music",
+    icon: icons.youtubeMusic,
+  },
+  {
+    name: "YouTube Kids",
+    icon: icons.youtubeKids,
+  },
+];
+
+// Basic Settings
+export const BasicSettingsNav = [
+  {
+    name: "Settings",
+    icon: icons.settings,
+  },
+  {
+    name: "Report history",
+    icon: icons.reportHistory,
+  },
+  {
+    name: "Help",
+    icon: icons.help,
+  },
+  {
+    name: "Send feedback",
+    icon: icons.sendFeeback,
+  },
+];
+
+export const sidebarLinkGroup1 = [
+  "About",
+  "Press",
+  "Copyright",
+  "Contact us",
+  "Creators",
+  "Advertise",
+  "Developers",
+];
+
+export const sidebarLinkGroup2 = [
+  "Terms",
+  "Privacy",
+  "Policy & Safety",
+  "How YouTube works",
+  "Test new features",
+];
